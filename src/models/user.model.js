66784f8be38dc35f1d2b5c31fe317961a0a1bdc8
Soham553 <sakharesoham10@gmail.com>
2025-07-21 23:31:06 +0530

    JSW token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -22,7 +22,7 @@ const userSchema = new Schema({
     },
 
 
-    fullname : {
+    fullName : {
         type : String,
         required : true,
     },
@@ -63,5 +63,31 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
 
+userSchema.method.generateAccessToken = function (){
+    return jwt.sign(
+        {
+            _id : this.id,
+            email : this.email,
+            username : this.username,
+            fullname : this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+        }
+   )
+}
+userSchema.method.generateRefereshToken = function (){
+         return jwt.sign(
+        {
+            _id : this.id,
+           
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+   )
+}
 
 export const User = mongoose.model("User", userSchema);
